bdcbe175747921a1cd833fe132acc6aebdfaf4e4
joanneke <jodelajo@gmail.com>
2022-09-10 13:55:08 +0200

    create first screens and navigation

---
+++ screens/AllExpenses.js
@@ -0,0 +1,7 @@
+import {View, Text} from "react-native"
+
+export default function AllExpenses() {
+    return <View>
+        <Text>All expenses screen</Text>
+    </View>
+}
---
+++ screens/RecentExpense.js
@@ -0,0 +1,7 @@
+import {View, Text} from "react-native"
+
+export default function RecentExpenses() {
+    return <View>
+        <Text>Recent expenses screen</Text>
+    </View>
+}
