b6efcb8203f4cbe599511efe495f6b33f7ac3304
jodelajo <jodelajo@gmail.com>
2022-10-19 18:18:37 +0200

    repaire webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-    argv = {
-        mode: 'development'
-    }
-    console.log(argv)
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
   
